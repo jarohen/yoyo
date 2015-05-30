6edd9dd5e744cfcbfb418ab14f4a28c67a281db3
James Henderson <james@jarohen.me.uk>
2015-05-30 20:52:45 +0100

    Added a JDBC pool module

---
+++ jdbc-pool/src/yoyo/jdbc_pool.clj
@@ -0,0 +1,28 @@
+(ns yoyo.jdbc-pool
+  (:require [clojure.tools.logging :as log])
+  (:import [org.apache.commons.dbcp2 BasicDataSource]))
+
+(def known-drivers
+  {"postgresql" "org.postgresql.Driver"
+   "mysql" "com.mysql.jdbc.Driver"
+   "mssql" "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+   "odbc" "sun.jdbc.odbc.JdbcOdbcDriver"
+   "sqlite" "org.sqlite.JDBC"
+   "h2" "org.h2.Driver"})
+
+(defn with-db-pool [{:keys [driver subprotocol host port username password db max-total max-idle] :as db-config} f]
+  (log/info "Starting JDBC pool...")
+
+  (let [pool {:datasource (doto (BasicDataSource.)
+                            (.setDriverClassName (or driver (get known-drivers subprotocol)))
+                            (.setAccessToUnderlyingConnectionAllowed true)
+                            (.setUrl (format "jdbc:%s://%s:%s/%s" subprotocol host port db))
+                            (.setUsername username)
+                            (.setPassword password)
+                            (cond-> max-total (.setMaxTotal max-total))
+                            (cond-> max-idle (.setMaxIdle max-idle)))}]
+    (try
+      (f pool)
+
+      (finally
+        (.close pool)))))
