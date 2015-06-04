23cc5f5d6c93ac508a870d75fcb10606961a05de
James Henderson <james@jarohen.me.uk>
2015-06-04 08:51:12 +0100

    Adding an http-kit module

---
+++ http-kit/project.clj
@@ -0,0 +1,10 @@
+(defproject jarohen/yoyo.http-kit "0.0.1"
+  :description "FIXME: write description"
+  :url "https://github.com/james-henderson/yoyo"
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [http-kit "2.1.18"]
+                 [org.clojure/tools.logging "0.3.1"]])
---
+++ http-kit/src/yoyo/http_kit.clj
@@ -0,0 +1,19 @@
+(ns yoyo.http-kit
+  (:require [org.httpkit.server :as http]
+            [clojure.tools.logging :as log]))
+
+(defn with-webserver [{:keys [handler port httpkit-opts] :as this} f]
+  (log/infof "Starting web server on port %d..." port)
+  (let [stop-server! (http/run-server (some-fn handler
+                                               (constantly {:status 404
+                                                            :body "Not found"}))
+
+                                      (merge {:port port}
+                                             httpkit-opts))]
+    (log/info "Started web server.")
+    (try
+      (f)
+      (finally
+        (log/info "Stopping web server...")
+        (stop-server!)
+        (log/info "Stopped web server.")))))
