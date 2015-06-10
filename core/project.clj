3b7281f0095bce9c8d338d8fef6f253c14227d7b
James Henderson <james@jarohen.me.uk>
2015-06-10 19:43:09 +0100

    Bumping to SNAPSHOT

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/yoyo "0.0.1"
+(defproject jarohen/yoyo "0.0.2-SNAPSHOT"
   :description ""
   :url "https://github.com/james-henderson/yoyo"
   :license {:name "Eclipse Public License"
