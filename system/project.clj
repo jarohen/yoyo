a4d41520f0a763a1ba2fc7ca2257f5e860188c79
James Henderson <james@jarohen.me.uk>
2015-07-03 22:56:09 +0100

    Updating 'with' -> 'using', and schemaifying

--- system/project.clj
+++ system/project.clj
@@ -3,7 +3,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [medley "0.6.0"]
-                 [com.stuartsierra/dependency "0.1.1"]]
+                 [com.stuartsierra/dependency "0.1.1"]
+                 [prismatic/schema "0.4.3"]]
 
   :profiles {:dev {:dependencies [[jarohen/yoyo "0.0.3"]
                                   [com.stuartsierra/component "0.2.3"]]}})
