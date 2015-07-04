4c31474191081e226f4332526bf4354e404d0484
James Henderson <james@jarohen.me.uk>
2015-07-04 22:52:31 +0100

    Adding in a simple blog schema

--- samples/blog/src/blog/service/datomic.clj
+++ samples/blog/src/blog/service/datomic.clj
@@ -1,7 +1,15 @@
 (ns blog.service.datomic
   (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
             [datomic.api :as d]))
+
+(defn assert-schema! [datomic-conn]
+  (log/info "Migrating Datomic database...")
+  @(d/transact datomic-conn
+               (read-string (slurp (io/resource "datomic-schema.edn"))))
+  (log/info "Migrated Datomic database."))
 
 (defn open-datomic-conn [{{:keys [uri]} :datomic-opts}]
   (d/create-database uri)
-  (d/connect uri))
+  (doto (d/connect uri)
+    assert-schema!))
