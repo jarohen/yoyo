1d05e9ba1fc1cc24abc8e7459338f11268129709
James Henderson <james@jarohen.me.uk>
2015-07-04 18:39:39 +0100

    [aleph] Updating Aleph component to better fit with upcoming 'system' - 0.0.2

--- aleph/project.clj
+++ aleph/project.clj
@@ -1,10 +1,7 @@
-(defproject jarohen/yoyo.aleph "0.0.1"
-  :description "FIXME: write description"
-  :url "https://github.com/james-henderson/yoyo"
-
+(defproject jarohen/yoyo.aleph "0.0.2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [aleph "0.4.0"]
                  [org.clojure/tools.logging "0.3.1"]])
--- aleph/src/yoyo/aleph.clj
+++ aleph/src/yoyo/aleph.clj
@@ -2,14 +2,13 @@
   (:require [aleph.http :as http]
             [clojure.tools.logging :as log]))
 
-(defn with-webserver [{:keys [handler port aleph-opts] :as this} f]
+(defn with-server [{:keys [handler server-opts]} f]
   (log/infof "Starting web server on port %d..." port)
   (let [server (http/start-server (some-fn handler
                                            (constantly {:status 404
                                                         :body "Not found"}))
 
-                                  (merge {:port port}
-                                         aleph-opts))]
+                                  server-opts)]
     (log/info "Started web server.")
     (try
       (f server)
