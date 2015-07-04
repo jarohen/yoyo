71c93d3f4aa7f632fe1f1bbbbf57310d9b93ebbe
James Henderson <james@jarohen.me.uk>
2015-07-04 18:42:57 +0100

    [http-kit] Updating http-kit component to better fit with upcoming 'system' - 0.0.3

--- http-kit/project.clj
+++ http-kit/project.clj
@@ -1,10 +1,9 @@
-(defproject jarohen/yoyo.http-kit "0.0.2"
-  :description "FIXME: write description"
+(defproject jarohen/yoyo.http-kit "0.0.3"
   :url "https://github.com/james-henderson/yoyo"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [http-kit "2.1.18"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [http-kit "2.1.19"]
                  [org.clojure/tools.logging "0.3.1"]])
--- http-kit/src/yoyo/http_kit.clj
+++ http-kit/src/yoyo/http_kit.clj
@@ -2,17 +2,16 @@
   (:require [org.httpkit.server :as http]
             [clojure.tools.logging :as log]))
 
-(defn with-webserver [{:keys [handler port httpkit-opts] :as this} f]
+(defn with-server [{:keys [handler server-opts] :as http-kit-opts} f]
   (log/infof "Starting web server on port %d..." port)
   (let [stop-server! (http/run-server (some-fn handler
                                                (constantly {:status 404
                                                             :body "Not found"}))
 
-                                      (merge {:port port}
-                                             httpkit-opts))]
+                                      server-opts)]
     (log/info "Started web server.")
     (try
-      (f this)
+      (f http-kit-opts)
       (finally
         (log/info "Stopping web server...")
         (stop-server!)
