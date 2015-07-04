b6f0a540c7f4961b4d1f78b782e7e0163a0c8f56
James Henderson <james@jarohen.me.uk>
2015-07-04 18:29:15 +0100

    Bumping system to 0.0.1-alpha1

--- system/project.clj
+++ system/project.clj
@@ -1,10 +1,9 @@
-(defproject jarohen/yoyo.system "0.0.1-SNAPSHOT"
+(defproject jarohen/yoyo.system "0.0.1-alpha1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [medley "0.6.0"]
                  [com.stuartsierra/dependency "0.1.1"]
                  [prismatic/schema "0.4.3"]]
 
-  :profiles {:dev {:dependencies [[jarohen/yoyo "0.0.3"]
-                                  [com.stuartsierra/component "0.2.3"]]}})
+  :profiles {:dev {:dependencies [[jarohen/yoyo "0.0.4"]]}})
