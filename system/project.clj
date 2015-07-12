87efc972724e103b98400db898a097c86d7ef7b6
James Henderson <james@jarohen.me.uk>
2015-07-12 14:07:05 +0100

    Adding the necessaries for an initial release

--- system/project.clj
+++ system/project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/yoyo.system "0.0.1-alpha1"
+(defproject jarohen/yoyo.system "0.0.1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
