7f94f3f521dfaa0aec756ad1667661cf20a95ddf
James Henderson <james@jarohen.me.uk>
2015-08-17 21:42:05 +0100

    [system] How did I forget a 'cats' dep here?

--- system/project.clj
+++ system/project.clj
@@ -3,4 +3,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [medley "0.6.0"]
+                 [funcool/cats "0.6.1"]
                  [jarohen/yoyo "0.0.6-SNAPSHOT"]])
