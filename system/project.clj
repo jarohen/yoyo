5167b1416fd21497f00e36246f5b52fb2fb5699e
James Henderson <james@jarohen.me.uk>
2015-08-23 17:14:13 +0100

    Need to manually qualify `::ys/` in y.s.run

--- system/project.clj
+++ system/project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/yoyo.system "0.0.2-SNAPSHOT"
+(defproject jarohen/yoyo.system "0.0.2-alpha2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
