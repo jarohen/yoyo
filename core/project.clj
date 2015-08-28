868b5772d688e8a2f489185aff5ccb1efd67558e
James Henderson <james@jarohen.me.uk>
2015-08-28 20:34:46 +0100

    Back to SNAPSHOT

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/yoyo "0.0.6-alpha3"
+(defproject jarohen/yoyo "0.0.6-SNAPSHOT"
   :description ""
   :url "https://github.com/james-henderson/yoyo"
   :license {:name "Eclipse Public License"
